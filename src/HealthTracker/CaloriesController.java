9dc2bac31e5d4e60added5b5c291c3b889dde30a
jewit <jewitt9876@gmail.com>
2021-05-10 18:59:58 +0100

    updated calorie log display

--- src/HealthTracker/CaloriesController.java
+++ src/HealthTracker/CaloriesController.java
@@ -1,4 +1,13 @@
 package HealthTracker;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import org.w3c.dom.Text;
+
+import java.awt.*;
+
 public class CaloriesController {
+
 }
