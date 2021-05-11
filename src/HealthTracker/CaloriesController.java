3e61d5bf06655cc04dcba5ca26543b6c06759b47
jewit <jewitt9876@gmail.com>
2021-05-11 16:30:57 +0100

    updated calorie log display and add image

--- src/HealthTracker/CaloriesController.java
+++ src/HealthTracker/CaloriesController.java
@@ -1,13 +1,65 @@
 package HealthTracker;
 
 import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import org.w3c.dom.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Line;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 import java.awt.*;
 
 public class CaloriesController {
+
+    @FXML
+    private AnchorPane a_pane;
+
+    @FXML
+    private AnchorPane banner;
+
+    @FXML
+    private Text track;
+
+    @FXML
+    private Text p_name;
+
+    @FXML
+    private Button btn_group;
+
+    @FXML
+    private Button btn_workouts;
+
+    @FXML
+    private Button btn_home;
+
+    @FXML
+    private Button btn_user;
+
+    @FXML
+    private Button btn_settings;
+
+    @FXML
+    private Pane content;
+
+    @FXML
+    private Line line;
+
+    @FXML
+    private Button submit;
+
+    @FXML
+    private TextField text_field;
+
+    @FXML
+    private Label heading;
+
+    @FXML
+    private Label sub_text;
+
+    @FXML
+    private ImageView img;
 
 }
