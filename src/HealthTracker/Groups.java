1c71de7c2b611987cb7bae484b5ae0bde5e87d78
Vailey Ong <58063841+VaileyXO@users.noreply.github.com>
2021-05-12 05:09:57 +0100

    Update Groups.java

--- src/HealthTracker/Groups.java
+++ src/HealthTracker/Groups.java
@@ -12,13 +12,6 @@ public class Groups extends Application {
     @Override
     @FXML
     public void start(Stage primaryStage) throws Exception{
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("groups.fxml"));
-    //      Parent root = loader.load();
-//        Scene scene = new Scene(root);
-//        primaryStage.setTitle("Tracker");
-//        primaryStage.setScene(scene);
-      //  primaryStage.setScene(new Scene(root, 400, 700));
-//        primaryStage.show();
         Parent root = FXMLLoader.load(getClass().getResource("groups.fxml"));
         primaryStage.setTitle("TRACK");
         primaryStage.setScene(new Scene(root,400,700));
